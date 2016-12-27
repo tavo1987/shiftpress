22f6cc01a4a1133d67cd30ad1e7cfe4c68b692dd
tavo1987 <tavo198718@gmail.com>
2016-12-27 17:16:46 -0500

    Update header

--- partials/entry.php
+++ partials/entry.php
@@ -1,4 +1,6 @@
-<h2>desde entrada</h2>
+
+<h2>Content</h2>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header>
         <?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> <?php edit_post_link(); ?>
