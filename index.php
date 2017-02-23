95a08126127a5c56a7e0da6f2bf2544467628827
tavo <tavo198718@gmail.com>
2017-02-22 22:05:45 -0500

    Added function for pagination posts

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <?php get_template_part( 'partials/entry' ); ?>
             <?php comments_template(); ?>
-        <?php endwhile; endif; ?>
+        <?php endwhile;?>
+            <!-- post navigation -->
+            <div class="pagination">
+                <?php if (function_exists('wp_corenavi')) wp_corenavi(); ?>
+            </div>
+        <?php endif; ?>
         <?php get_template_part( 'partials/nav', 'below' ); ?>
     </section>
     <div class="small-12 medium-12 large-3">
